0dda306d5f296fb60dd394a0a0dd310d1e380c54
David Driscoll <david.driscoll@gmail.com>
2018-06-28 10:26:29 -0400

    fixed resolve signature to work with parameters

--- src/decorators/resolve.ts
+++ src/decorators/resolve.ts
@@ -6,16 +6,28 @@ import { decorateParameterOrProperty } from './decorateParameterOrProperty';
  *
  * @export
  */
+
+export function Resolve(): (
+    target: Object,
+    propertyOrParameterName: string | symbol,
+    index?: number
+) => void;
 export function Resolve(target: Object, propertyKey: string | symbol): void;
-export function Resolve(key?: Key<any>): PropertyDecorator;
-export function Resolve(targetOrKey?: Object | Key<any>, propertyKey?: string | symbol) {
+export function Resolve(
+    key?: Key<any>
+): (target: Object, propertyOrParameterName: string | symbol, index?: number | undefined) => void;
+export function Resolve(
+    targetOrKey?: Object | Key<any>,
+    propertyKey?: string | symbol,
+    index?: number
+) {
     const deco = (k?: Key<any>) => decorateParameterOrProperty(x => k || x, 'resolve');
     if (propertyKey == null) {
         return deco(targetOrKey as any);
     }
 
     // tslint:disable-next-line:no-non-null-assertion
-    return deco()(targetOrKey!, propertyKey);
+    return deco()(targetOrKey!, propertyKey, index);
 }
 
 export const inject = Resolve;
