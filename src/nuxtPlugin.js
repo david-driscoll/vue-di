eafa8cbacad29ba9f60fc027c2cf90af9641e858
David Driscoll <david.driscoll@gmail.com>
2019-05-12 11:30:33 -0400

    reigster store as expecteD

--- src/nuxtPlugin.js
+++ src/nuxtPlugin.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import { Container } from 'vue-di';
 import VueDi from 'vue-di/vue';
+import { Store } from 'vuex'
 
 export default (context, inject) => {
     const container = Container.instance || new Container();
     Vue.use(VueDi, { container });
     container.makeGlobal();
     context.container = context.app.container = container;
+    container.registerInstance(Store, context.store);
 }
